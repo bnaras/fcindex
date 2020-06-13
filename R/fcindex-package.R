3db67d9fc7b3b05277015d9a97852f5450335498
Balasubramanian Narasimhan <b.naras@gmail.com>
2020-06-12 19:27:56 -0700

    First release of fcindex as a package
    Passes CRAN checks

---
+++ R/fcindex-package.R
@@ -0,0 +1,13 @@
+#' Fast concordance-index computation
+#'
+#' This package provides one function `CIndex`, that implements a fast
+#' and efficient algorithm for computing concordance.
+#' @name fcindex-package
+#' @docType package
+#' @useDynLib fcindex, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#' @keywords survival regression package
+NULL
+
+
+
